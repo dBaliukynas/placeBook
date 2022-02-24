c8563808d669d50a5988e5ef486dd6ce02f28261
Domantas Baliukynas <domantas.baliukynas@mif.stud.vu.lt>
2022-02-25 00:46:13 +0200

    Improve homepage layout

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -3,17 +3,97 @@ import ReactDOM from "react-dom";
 
 function Home() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Home Component</div>
+        <div className="content-wrapper">
+            <div className="top-main-container center">
+                <div
+                    style={{
+                        width: "80%",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    <h1>Browse places to rent...</h1>
+                    <h2>or lease them </h2>
+                </div>
+            </div>
+            <div className="main-container">
+                <h3>Select a place by group</h3>
+                <div className="card-wrapper">
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        
     );
 }
 
