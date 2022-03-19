6a8cbd134ea2ae1aad0e98bd271ea19c25b3c653
Domantas Baliukynas <domantas.baliukynas@mif.stud.vu.lt>
2022-03-19 13:35:53 +0200

    Refactor route code

--- resources/js/Main.jsx
+++ resources/js/Main.jsx
@@ -4,13 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import RequireAuth from "./components/RequireAuth";
 import Redirecting from "./pages/Redirecting";
 import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import Property from "./pages/Property";
-import PropertyListing from "./pages/PropertyListing";
-import PropertySearch from "./pages/PropertySearch";
 import NotFound from "./components/NotFound";
+import RoutesForBreadcrumb from "./components/RoutesForBreadcrumb";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -25,34 +20,32 @@ const Main = () => {
                             <NotFound status={404} message="Page not found" />
                         }
                     />
-                    <Route path="/" element={<Home />}></Route>
-                    <Route path="about" element={<About />}></Route>
-
-                    <Route
-                        path="property-listing"
-                        element={
-                            <RequireAuth>
-                                <PropertyListing />
-                            </RequireAuth>
-                        }
-                    ></Route>
-
-                    <Route
-                        path="property-search"
-                        element={<PropertySearch />}
-                    ></Route>
-                    <Route
-                        path="profile"
-                        element={
-                            <RequireAuth>
-                                <Profile />
-                            </RequireAuth>
-                        }
-                    ></Route>
-                    <Route path="property/:id" element={<Property />}></Route>
+                    {RoutesForBreadcrumb().map((route, index) =>
+                        route.isAuthRequired ? (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <RequireAuth>{<route.component />}</RequireAuth>
+                                }
+                            />
+                        ) : (
+                            <Route
+                            key={index}
+                                path={route.path}
+                                element={<route.component />}
+                            />
+                        )
+                    )}
                 </Route>
-                <Route path="login" element={<Redirecting pathName="login" />}></Route>
-                <Route path="register" element={<Redirecting pathName="register" />}></Route>
+                <Route
+                    path="login"
+                    element={<Redirecting pathName="login" />}
+                ></Route>
+                <Route
+                    path="register"
+                    element={<Redirecting pathName="register" />}
+                ></Route>
             </Routes>
         </Router>
     );
