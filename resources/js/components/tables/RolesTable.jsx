d4fc943925a208349d5555f2c73a7b6adcc99832
Domantas Baliukynas <domantas.baliukynas@mif.stud.vu.lt>
2022-04-20 22:39:06 +0300

    Tweak user creation in front-end

--- resources/js/components/tables/RolesTable.jsx
+++ resources/js/components/tables/RolesTable.jsx
@@ -135,7 +135,6 @@ const RolesTable = (props) => {
                 buttonText="Delete"
                 title="Delete role"
                 buttonType="btn-danger"
-                onClick={deleteRole}
                 body={
                     <>
                         <p>
